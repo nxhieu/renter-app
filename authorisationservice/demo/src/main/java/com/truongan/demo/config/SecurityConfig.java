0649f5287ff98c874d42856b74a11f048a64ba7c
Hieu Nguyen <nxhieuqn@gmail.com>
2020-07-01 17:47:07 +1000

    merge with master

--- authorisationservice/demo/src/main/java/com/truongan/demo/config/SecurityConfig.java
+++ authorisationservice/demo/src/main/java/com/truongan/demo/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticationEntryPoint(new RestAuthenticationEntryPoint()).and().authorizeRequests()
                 .antMatchers("/", "/error", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg",
                         "/**/*.html", "/**/*.css", "/**/*.js")
-                .permitAll().antMatchers("/auth/**", "/oauth2/**").permitAll().anyRequest().authenticated();
-
+                .permitAll().antMatchers("/auth/**", "/oauth2/**", "/callBack").permitAll().anyRequest()
+                .authenticated();
     }
 }
